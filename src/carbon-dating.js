205a2bc11aa628ccfc86e8a2400db33072f01fb6
dmp11 <pd8@mail.ru>
2021-03-27 22:08:12 +0300

    feat: implement carbon dating subtask

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,13 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  
+  if (typeof(sampleActivity)==="string" && isFinite(sampleActivity) && sampleActivity > 0 && sampleActivity <= 15) {
+        let sA = parseFloat(sampleActivity);
+        let t = Math.log(MODERN_ACTIVITY/(sA))/(.693/HALF_LIFE_PERIOD);
+        return Math.ceil(t).toString();
+  }
+  else return false;
+
 };
