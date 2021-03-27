9c495f99dd638803243e48df0187700c99fc5fe3
dmp11 <pd8@mail.ru>
2021-03-27 14:23:49 +0300

    feat: implement count cats subtask

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(mask) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  const arrCats = mask.toString().split(",");
+  let numCats = 0;
+  for (let i = 0; i < arrCats.length; i++) {
+    if (arrCats[i] === "^^") {
+      numCats++;
+    }
+  }
+  return numCats;
 };
