b1f0e0dc2f304e5f3d9121a2cc375fe8df64f028
dmp11 <pd8@mail.ru>
2021-03-28 17:21:58 +0300

    feat: implement dream team subtask

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(ar) {
+  let newAr;
+  let str = '';
+  if (Array.isArray(ar)) {
+    for (let i = 0; i < ar.length; i++) {
+      if (typeof ar[i] === "string") {
+        newAr = ar[i].trim().split('');
+        str += newAr[0];
+      }
+    }
+    return str.toUpperCase().split('').sort().join('');
+  } else return false;
 };
